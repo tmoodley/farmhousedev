c3f56d62eabe11537b03ce1a96cb06aa98de97ec
ty_moodley <ty.moodley@gmail.com>
2016-12-18 15:53:12 -0500

    added viewcomponent.

---
+++ src/FarmHouseDeliveryApp/ViewComponents/DepartmentViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using FarmHouseDeliveryApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmHouseDeliveryApp.ViewComponents
+{
+    public class DepartmentViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DepartmentViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Department.ToList());
+        }
+    }
+}
