d241506bfc1a6082daa51b9b44153a5e8808f43f
ty_moodley <ty.moodley@gmail.com>
2016-12-19 02:05:26 -0500

    Added product page.

---
+++ src/FarmHouseDeliveryApp/ViewComponents/PromoViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using FarmHouseDeliveryApp.Data;
+using FarmHouseDeliveryApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FarmHouseDeliveryApp.ViewComponents
+{
+    public class PromoViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PromoViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var products = _context.Product.Take(4).ToList();
+
+            return View(products);
+        }
+    }
+}
